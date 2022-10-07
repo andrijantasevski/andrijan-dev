cc1f8fe3a1770df0bb54a4c961d808a727596a5d
Andrijan Tasevski <andrijantasevski@gmail.com>
2022-10-07 19:38:01 +0200

    Initial folder structure committed.

---
+++ README.md
@@ -0,0 +1,1 @@
+# andrijan-dev
---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()]
+});
