35a58f7473af052557349da9278f2f8ac5764939
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-01-12 22:46:58 +0100

    Added sitemap to site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,27 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), mdx(), image()],
+  site: "https://andrijan.dev/",
+  integrations: [
+    react(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+    image(),
+    sitemap(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: "css-variables"
-    }
-  }
+      theme: "css-variables",
+    },
+  },
 });
